7389283ceb6deb5a5e131b95c41cac0742133a8a
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2022-03-26 00:04:29 +0900

    [vim] Cleanup of plugins
    - Remove completion plugins since it's not working anyway
    - Remove rust plugin as I haven't written rust in a year
    - Remove the terraform plugin as its value seems rather minimal
    - Document what which-key does
    - Remove folke/trouble as I'm not really using it
    + Add color to NERDTree (filename and icon) based on filetype

--- config/nvim/lua/kinbiko/plugins.lua
+++ config/nvim/lua/kinbiko/plugins.lua
@@ -17,7 +17,7 @@ require('packer').startup(function(use)
   use 'haya14busa/vim-asterisk' -- Use * without moving immediately
   use 'junegunn/fzf.vim' -- The lightning fast fzf fuzzy finder
   use 'airblade/vim-gitgutter' -- Makes the git gutter with +, ~, and - available
-  use 'tpope/vim-fugitive' -- Most git features available through :G*
+  use 'tpope/vim-fugitive' -- Most git features available through :Git foo
   use 'rhysd/committia.vim' -- Running 'git commit' on the command line enables diff and other niceties
   use 'tpope/vim-surround' -- ysiw syntax for surrounding
   use 'tpope/vim-repeat' -- Make vim-surround things repeatable with .
@@ -25,48 +25,25 @@ require('packer').startup(function(use)
   use 'christoomey/vim-tmux-navigator' -- Work better with tmux
   use 'machakann/vim-highlightedyank' -- Highlight when yanking so you don't need to pop into visual mode constantly
   use 'sickill/vim-pasta' -- Context aware pasting + indentation
-  use 'ryanoasis/vim-devicons' -- Pretty icons per filetype. Must be loaded after NERDTree.
+  use 'ryanoasis/vim-devicons' -- Pretty icons per filetype. Must be loaded after NERDTree. Unfortunate duplication of kyazdani42/nvim-web-devicons
+  use 'tiagofumo/vim-nerdtree-syntax-highlight' -- Add color to the filenames and icons in NERDTree
   use 'folke/twilight.nvim' -- "reading" mode that disables distant syntax highlighting
   use 'nacro90/numb.nvim' -- Preview the line while typing :<number>
   use 'mattn/emmet-vim' -- Shortcuts for creating html/jsx boilerplate
   use 'prettier/vim-prettier' -- Prettier formatter
   use 'elzr/vim-json' -- JSON syntax (error) highlighting + concealment
-  use 'rust-lang/rust.vim'--  Official Rust plugin.
-  use 'hashivim/vim-terraform' --  Terraform support
   use 'pangloss/vim-javascript'-- Syntax highlighting and concealment for JavaScript
   use 'maxmellon/vim-jsx-pretty' --  Make JSX look good (technically this supports TSX too, but has perf issues)
   use 'leafgarland/typescript-vim' -- Typescript syntax highlighting
   use 'peitalin/vim-jsx-typescript' -- TSX syntax highlighting without the perf issues
   use 'ray-x/lsp_signature.nvim' -- Keep the function signature docs up while filling in params
+  use 'folke/which-key.nvim' -- Reminders of which key-bindings are available if getting stuck half-way through a binding
 
   use { 'junegunn/fzf', run = vim.fn['fzf#install'] } -- Install the fzf binary as well
+  use { 'fatih/vim-go', run = ':GoUpdateBinaries' } -- Make vim good with Go
 
-  -- Make vim good with Go
-  use {
-    'fatih/vim-go',
-    run = ':GoUpdateBinaries'
-  }
   use 'neovim/nvim-lspconfig' -- Easy LSP configuration
   use 'williamboman/nvim-lsp-installer' -- Install LSP servers on demand with LSPInstall
-
-  -- This is a bit excessive: Only importing to avoid having documentation show
-  -- up in a nofile buffer (instead of a quickfix window) that I have to close
-  -- after each completion. If I could figure out how to make this window a
-  -- floating window or a quickfix window then I might not care about this
-  -- particular plugin
-  use 'nvim-lua/completion-nvim'
-
-
-  -- This is a big fat plugin that I'm really only using to make autocompletion useful.
-  use { 'ms-jpq/coq_nvim', branch='coq' }
-
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({})
-    end
-  }
 
   use {
     'nvim-lualine/lualine.nvim',
@@ -81,5 +58,4 @@ require('packer').startup(function(use)
     }
   }
 
-  use "folke/which-key.nvim"
 end)
