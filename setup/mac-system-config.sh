191a7557ae03ecc4124439e1439593a5224f8ebb
kinbiko <roger@mercari.com>
2022-09-18 01:25:18 +0900

    [anki] Sysconfig for making anki-connect work well

--- setup/mac-system-config.sh
+++ setup/mac-system-config.sh
@@ -7,3 +7,8 @@ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # Hide fluffy directories from finder that I can't delete
 chflags hidden Applications Movies Music Pictures Public
+
+# Make Anki-Connect work well even when not in foreground
+defaults write net.ankiweb.dtop NSAppSleepDisabled -bool true
+defaults write net.ichi2.anki NSAppSleepDisabled -bool true
+defaults write org.qt-project.Qt.QtWebEngineCore NSAppSleepDisabled -bool true
