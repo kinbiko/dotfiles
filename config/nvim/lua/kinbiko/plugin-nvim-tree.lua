ae5cec11bef7072a5979f15a7335f5aeea9b9663
kinbiko <roger@mercari.com>
2022-09-23 19:09:46 +0900

    [vim] Remove deprecated config

--- config/nvim/lua/kinbiko/plugin-nvim-tree.lua
+++ config/nvim/lua/kinbiko/plugin-nvim-tree.lua
@@ -26,7 +26,6 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     adaptive_size = false,
     centralize_selection = true,
     width = 30,
-    height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
