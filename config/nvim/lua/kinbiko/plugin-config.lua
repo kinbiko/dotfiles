67b2880b1234c1135a501377d847ec366e183990
kinbiko <roger@mercari.com>
2022-12-17 15:31:07 +0900

    feat(vim): configure treesitter to get prettier highlighting

--- config/nvim/lua/kinbiko/plugin-config.lua
+++ config/nvim/lua/kinbiko/plugin-config.lua
@@ -359,3 +359,26 @@ vim.g.tmux_navigator_disable_when_zoomed = 1
 -- space like this: [  ]. (should only have a single space inside when hitting
 -- space)
 vim.g.lexima_enable_space_rules = 0
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "c", "go", "javascript", "markdown", "typescript", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
