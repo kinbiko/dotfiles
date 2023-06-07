9e40d0c7c9dd0d457e838ab2203463e531b0f6d2
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2023-06-08 01:57:33 +0900

    chore(vim): Remove unnecessary colorscheme and remove options that don't work

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -55,7 +55,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
