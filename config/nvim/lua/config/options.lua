88a36dacac19479d1bf7d2e26aec9cc2b5478f2d
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2023-06-08 01:57:33 +0900

    chore(vim): Remove unnecessary colorscheme and remove options that don't work

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,18 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- LazyVim sets this to 'unnamedplus' which pollutes the system clipboard for every yank/delete/etc.
--- This unsets this.
-vim.opt.clipboard = ""
-
 vim.cmd.lang("en_US.UTF-8") -- Setting this explicitly means I can copy Japanese to the clipboard without mojibake
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Make normal text transparent
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Make normal text in non-current windows transparent
-vim.api.nvim_set_hl(0, "Visual", { bg = "none", fg = "yellow" }) -- Make visually selected text yellow
 
 local o = vim.o
 
+o.clipboard = "" -- LazyVim sets this to 'unnamedplus' which pollutes the system clipboard for every yank/delete/etc.
 o.softtabstop = 2 -- columns per tab key press in insert mode. Also on backspace
 o.scrolloff = 8 -- Number of lines from vertical edge to start scroll
 o.sidescrolloff = 15 -- Number of lines from horizontal edge to start scroll
