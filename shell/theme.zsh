7d74d5f4fba9371f1dd5d7ee9032c5a6f7bac328
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2023-06-10 20:40:53 +0900

    fix(vim): Refactor theme logic and fix broken color escape

--- shell/theme.zsh
+++ shell/theme.zsh
@@ -4,27 +4,37 @@ autoload -U colors && colors
 # Enable ls colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
-typeset +H _current_dir="%{$fg_bold[blue]%}%3~%{$reset_color%}"
-typeset +H _return_status=" %{$fg_bold[red]%}%(?..☠︎☠︎☠︎)%{$reset_color%}"
+function colorize() {
+  echo "%{$fg_bold[$2]%}$1%{$reset_color%}"
+}
 
-function git_prompt_info() {
+function git_info() {
   if ! GIT_OPTIONAL_LOCKS=0 command git rev-parse --git-dir &> /dev/null; then
     return 0
   fi
 
-  if [[ -n $(GIT_OPTIONAL_LOCKS=0 command git status --porcelain 2> /dev/null | tail -n 1) ]]; then
-    echo "%{$fg[yellow]%}⎇ ${$(GIT_OPTIONAL_LOCKS=0 command git symbolic-ref --short HEAD 2> /dev/null || echo "<<not on a branch>>")}%{$reset_color%} "
+  local is_dirty=$(GIT_OPTIONAL_LOCKS=0 command git status --porcelain 2> /dev/null | tail -n 1)
+  if [[ -n $is_dirty ]]; then
+    local color="yellow"
   else
-    echo "%{$fg[green]%}⎇ ${$(GIT_OPTIONAL_LOCKS=0 command git symbolic-ref --short HEAD 2> /dev/null || echo "<<not on a branch>>")}%{$reset_color%} "
+    local color="green"
   fi
+
+  local branch=$(GIT_OPTIONAL_LOCKS=0 command git symbolic-ref --short HEAD 2> /dev/null || echo "<<not on a branch>>")
+  echo "$(colorize "⎇ ${branch}" $color)"
 }
 
+local current_dir="$(colorize "%3~" "cyan")"
+local return_status_symbols="%(?..%? 💀)"
+local return_status_indicator=$(colorize "${return_status_symbols}" "red")
+local lambda_prompt="$(colorize "λ" "white")"
+
 # Main prompt
-PROMPT='$(git_prompt_info)${_current_dir}${_return_status}
-${fg[white]}λ${fg[yellow]}%{$reset_color%} '
+PROMPT='$(git_info) ${current_dir} ${return_status_indicator}
+${lambda_prompt} '
 
 # This shows up when a command runs longer than one line
-PROMPT2='${fg[white]}︙%{$reset_color%}'
+PROMPT2=$(colorize "︙" "black")
 
 # Colorize completions using default `ls` colors.
 zstyle ':completion:*' list-colors ''
