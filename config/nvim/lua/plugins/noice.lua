fa61180e6daebdfc17f8eb16ece5e477ffd51453
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2023-06-10 16:29:30 +0900

    fix(vim): Be less 'noicy'

---
+++ config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "\\<.*\\>" }, -- * and # usage
+              { find = "/.*" }, -- starting a search with /
+              { find = ".*Pattern not found.*" }, -- not finding anything with a search
+            },
+          },
+          view = "mini",
+        },
+      },
+    },
+  },
+}
