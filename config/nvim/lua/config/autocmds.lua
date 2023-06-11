65c1a453b2ed87023d3906fc6c62944a0b5b1dc6
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2023-06-11 20:44:39 +0900

    feat(vim): Tell me what's wrong when hovering over an issue

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+require("lazyvim.util").on_attach(function(_, buffer)
+  -- create the autocmd to show diagnostics
+  vim.api.nvim_create_autocmd("CursorHold", {
+    group = vim.api.nvim_create_augroup("_auto_diag", { clear = true }),
+    buffer = buffer,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = "rounded",
+        source = "always",
+        prefix = " ",
+        scope = "cursor",
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end,
+  })
+end)
