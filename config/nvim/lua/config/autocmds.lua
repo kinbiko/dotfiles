811408b62a0087533f73f23929acd3059b72abe1
kinbiko <roger@mercari.com>
2023-06-14 02:02:11 +0900

    fix(vim): Stop showing tabs in go files

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -2,21 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-require("lazyvim.util").on_attach(function(_, buffer)
-  -- create the autocmd to show diagnostics
-  vim.api.nvim_create_autocmd("CursorHold", {
-    group = vim.api.nvim_create_augroup("_auto_diag", { clear = true }),
-    buffer = buffer,
-    callback = function()
-      local opts = {
-        focusable = false,
-        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-        border = "rounded",
-        source = "always",
-        prefix = " ",
-        scope = "cursor",
-      }
-      vim.diagnostic.open_float(nil, opts)
-    end,
-  })
-end)
+-- Don't show tab character ghosts for go files
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.go" },
+  callback = function()
+    vim.opt.list = false
+  end,
+})
