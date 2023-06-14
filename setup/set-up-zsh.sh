bf07adf67b21facef9e0a2e26a2737c943c3da5f
kinbiko <roger.guldbrandsen@gmail.com>
2023-06-15 00:02:33 +0900

    chore(zsh): rename shell --> zsh

--- setup/set-up-zsh.sh
+++ setup/set-up-zsh.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-source "~/.config/shell/.zshenv"
+source "~/.config/zsh/.zshenv"
 # .zshenv contains $XDG_CONFIG_HOME so link only after this environment
 # variable is available
-ln -s "$XDG_CONFIG_HOME/shell/.zshenv" ~/.zshenv
+ln -s "$XDG_CONFIG_HOME/zsh/.zshenv" ~/.zshenv
 
 # Single quotes here is correct. Want to refer to the variable name in zsh, not
 # the value it has at the time of install.
-echo 'source $XDG_CONFIG_HOME/shell/.zshrc' >~/.zshrc
+echo 'source $XDG_CONFIG_HOME/zsh/.zshrc' >~/.zshrc
