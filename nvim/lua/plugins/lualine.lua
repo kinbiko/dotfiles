8a9e45e617ceaff0e09bc9d6abb7c5fb4a46b023
kinbiko <roger.guldbrandsen@gmail.com>
2023-07-01 01:27:31 +0900

    fix(vim): tweak status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,12 @@
 return {
-  -- statusline
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function()
       local icons = require("icons")
-      local Util = require("util")
 
       return {
         options = {
-          theme = "auto",
           globalstatus = true,
           disabled_filetypes = { statusline = { "dashboard" } },
         },
@@ -17,6 +14,10 @@ return {
           lualine_a = { "mode" },
           lualine_b = { "branch" },
           lualine_c = {
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1, symbols = { modified = "<unsaved>", readonly = "", unnamed = "" } },
+          },
+          lualine_x = {
             {
               "diagnostics",
               symbols = {
@@ -26,29 +27,6 @@ return {
                 hint = icons.diagnostics.Hint,
               },
             },
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-          },
-          lualine_x = {
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.command.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-              color = Util.fg("Statement"),
-            },
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.mode.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              color = Util.fg("Constant"),
-            },
-            -- stylua: ignore
-            {
-              function() return "  " .. require("dap").status() end,
-              cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-              color = Util.fg("Debug"),
-            },
-            { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.fg("Special") },
             {
               "diff",
               symbols = {
@@ -58,17 +36,13 @@ return {
               },
             },
           },
-          lualine_y = {
-            { "progress", separator = " ", padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
+          lualine_y = {},
           lualine_z = {
             function()
               return " " .. os.date("%R")
             end,
           },
         },
-        extensions = { "lazy" },
       }
     end,
   },
