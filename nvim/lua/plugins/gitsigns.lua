a14345591cc0a701a96a75d6632b103d69c9a89d
kinbiko <roger.guldbrandsen@gmail.com>
2023-10-22 00:57:40 +0900

    chore(vim): cleanup unused plugin mappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,19 +12,8 @@ return {
         untracked = { text = "?" },
       },
       on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
-        local function map(l, r, desc)
-          vim.keymap.set("n", l, r, { buffer = buffer, desc = desc })
-        end
-
-        map("<leader>gn", gs.next_hunk, "Next Hunk")
-        map("<leader>gp", gs.prev_hunk, "Previous Hunk")
-        map("<leader>ga", gs.stage_hunk, "Stage hunk")
-        map("<leader>gu", gs.reset_hunk, "Undo hunk")
-        map("<leader>gr", gs.reset_hunk, "Reset hunk")
-        map("<leader>gb", gs.blame_line, "Toggle blame")
-        map("<leader>gw", gs.preview_hunk_inline, "View hunk")
-        map("<leader>gd", gs.diffthis, "Diff This")
+        vim.keymap.set("n", "<leader>gu", package.loaded.gitsigns.reset_hunk, { buffer = buffer, desc = "Undo hunk" })
+        vim.keymap.set("n", "<leader>gb", package.loaded.gitsigns.blame_line, { buffer = buffer, desc = "Git Blame" })
       end,
     },
   },
--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -12,12 +12,10 @@ return {
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     keys = {
-      { "<leader>GAT", "<cmd>GoAddTag<cr>", desc = "Add Struct Tags" },
-      { "<leader>GFS", "<cmd>GoFillStruct<cr>", desc = "Fill Struct" },
-      { "<leader>GT", "<cmd>GoTest<cr>", desc = "go test ./..." },
-      { "<leader>GA", "<cmd>GoAlt<cr>", desc = "Open Alternative File" },
-      { "<leader>GE", "<cmd>GoIfErr<cr>", desc = "Open Alternative File" },
-      { "<leader>GR", "<cmd>GoGenReturn<cr>", desc = "Open Alternative File" },
+      { "<leader>gat", "<cmd>GoAddTag<cr>", desc = "Add Struct Tags" },
+      { "<leader>gfs", "<cmd>GoFillStruct<cr>", desc = "Fill Struct" },
+      { "<leader>gt", "<cmd>GoTest<cr>", desc = "go test ./..." },
+      { "<leader>ga", "<cmd>GoAlt<cr>", desc = "Open Alternative File" },
     },
   },
 }
