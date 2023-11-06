1a6fd0e947cb2c5b6e73540b1846c0be1068e399
kinbiko <roger.guldbrandsen@gmail.com>
2023-11-07 01:37:05 +0900

    feat(zsh): Add a 'checkout' function for checking out either a local branch or a github PR after selecting it in a fzf menu

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -3,3 +3,21 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --color=always --style=numbers --line-range=:500 {}"'
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+function checkout() {
+    local git_branches pr_num_title_branch view branch num
+
+    git_branches=$(git branch)
+    pr_num_title_branch=$(gh pr list --json 'title,number,headRefName' | jq '.[] | {number: .number|tostring, title, headRefName} | .number + "\t" + .title + "\t" + .headRefName')
+    view="$git_branches\n$pr_num_title_branch"
+    branch=$(echo $view | fzf --prompt="Select a branch: ")
+
+    if [[ -n "$branch" ]]; then
+        num=$(echo "$branch" | cut -f 1)
+        if [[ $num =~ ^[0-9]+$ ]] ; then
+            gh pr checkout $num
+        else
+            git checkout $(echo $branch | xargs)
+        fi
+    fi
+}
