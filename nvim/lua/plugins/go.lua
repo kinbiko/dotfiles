719ad90b2c1aab4558f6623206c7cc0658f6cca0
kinbiko <roger@mercari.com>
2023-12-13 23:13:58 +0900

    refactor(vim): go back to vim-go

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,20 +1,11 @@
 return {
   {
-    "ray-x/go.nvim", -- Go development features
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
+    "fatih/vim-go",
     ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     keys = {
       { "<leader>gat", "<cmd>GoAddTag<cr>", desc = "Add Struct Tags" },
       { "<leader>gfs", "<cmd>GoFillStruct<cr>", desc = "Fill Struct" },
-      { "<leader>gt", "<cmd>GoTest<cr>", desc = "go test ./..." },
+      { "<leader>gt", "<cmd>GoTest<cr>", desc = "Run Tests" },
       { "<leader>ga", "<cmd>GoAlt<cr>", desc = "Open Alternative File" },
     },
   },
