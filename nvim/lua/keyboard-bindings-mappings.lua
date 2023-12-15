a6518df54f36533986b1cd5ef8b2bb8a1ab1cea8
kinbiko <roger.guldbrandsen@gmail.com>
2023-12-15 14:52:03 +0900

    fix(vim): unbreak jump paragraphs and remove luasnip

--- nvim/lua/keyboard-bindings-mappings.lua
+++ nvim/lua/keyboard-bindings-mappings.lua
@@ -41,8 +41,8 @@ map(n, "\\", function() require("util").float_term(nil, { cwd = require("util").
 -- Note to the reader: I've programmed a second layer on my ergodox keyboard to
 -- map hjkl to the arrow keys. So even when I do use the arrow keys my fingers
 -- don't leave the home row.
-map(n, "<up>", ":keepjumps normal {zz") -- Previous paragraph
-map(n, "<down>", ":keepjumps normal }zz") -- Next paragraph
+map(n, "<up>", ":keepjumps normal {zz<cr>") -- Previous paragraph
+map(n, "<down>", ":keepjumps normal }zz<cr>") -- Next paragraph
 map(v, "<up>", "{zz") -- Previous paragraph
 map(v, "<down>", "}zz") -- Next paragraph
 map(n, "0", "^") -- Make 0 take me to the first non-blank character of the line.
