6397174cc75bf06c99c4f672f36835cbcb5a951c
kinbiko <roger.guldbrandsen@gmail.com>
2025-04-04 20:35:38 +0900

    fix(fzf): use the latest shell script from within fzf instead of the standalone script to source

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,7 +2,7 @@ export FZF_DEFAULT_COMMAND='rg --files-with-matches --hidden .'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --color=always --style=numbers --line-range=:500 {}"'
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source <(fzf --zsh)
 
 function checkout() {
     local git_branches pr_num_title_branch view branch num
