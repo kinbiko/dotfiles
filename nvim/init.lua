5fea48a8389530f5ef9fc134cdfa4ec8fbbb4e40
kinbiko <roger@mercari.com>
2025-04-18 19:24:03 +0900

    feat(vim): change theme based on system settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,15 @@ require("options")
 require("setup-plugins")
 require("autocmds")
 require("keyboard-bindings-mappings") -- a longer name makes it easier to find with Telescope
-vim.cmd.colorscheme("tokyonight-night")
+
+local function set_colorscheme()
+  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+  local result = handle:read("*a") -- NOTE: result is empty unless dark.
+  handle:close()
+
+  local theme = (result:match("Dark") and "tokyonight-night") or "tokyonight-day"
+  vim.cmd.colorscheme(theme)
+end
+
+set_colorscheme()
+vim.api.nvim_create_autocmd("FocusGained", { pattern = "*", callback = set_colorscheme })
