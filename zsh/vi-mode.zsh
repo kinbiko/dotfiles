3edf77b1366bf9a6fae9f2f27596c6c74869e12e
kinbiko <roger.guldbrandsen@gmail.com>
2025-04-18 21:40:01 +0900

    chore(zsh): remove shell bindings I don't use

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -74,22 +74,11 @@ autoload -Uz edit-command-line
 zle -N edit-command-line
 bindkey -M vicmd 'vv' edit-command-line
 
-# allow ctrl-p, ctrl-n for navigate history (standard behaviour)
-bindkey '^P' up-history
-bindkey '^N' down-history
-
-# allow ctrl-h, ctrl-w, ctrl-? for char and word deletion (standard behaviour)
-bindkey '^?' backward-delete-char
-bindkey '^h' backward-delete-char
+# allow ctrl-w for word deletion (standard behaviour)
 bindkey '^w' backward-kill-word
 
-# allow ctrl-r and ctrl-s to search the history
+# allow ctrl-r to search the history
 bindkey '^r' history-incremental-search-backward
-bindkey '^s' history-incremental-search-forward
-
-# allow ctrl-a and ctrl-e to move to beginning/end of line
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
 
 function wrap_clipboard_widgets() {
   # NB: Assume we are the first wrapper and that we only wrap native widgets
