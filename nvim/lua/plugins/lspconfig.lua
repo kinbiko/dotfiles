18c62c27d067f08bcfed8c99616d9e2263f7fd7a
kinbiko <roger.guldbrandsen@gmail.com>
2025-10-30 01:48:24 +0900

    fix(vim): Remove dependency on deprecated plugin

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -199,7 +199,6 @@ return {
       "mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
-      "jose-elias-alvarez/typescript.nvim",
       "b0o/SchemaStore.nvim",
       version = false, -- last release is way too old
     },
