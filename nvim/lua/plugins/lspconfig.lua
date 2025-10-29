176b87c7a4827f3137f05a10de7ef713e1a38701
kinbiko <roger.guldbrandsen@gmail.com>
2025-10-30 01:55:26 +0900

    fix(vim): avoid using internal APIs for mason-lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -388,28 +388,30 @@ return {
         require("lspconfig")[server].setup(server_opts)
       end
 
-      -- get all the servers that are available thourgh mason-lspconfig
+      -- Setup servers with mason-lspconfig
       local have_mason, mlsp = pcall(require, "mason-lspconfig")
-      local all_mslp_servers = {}
-      if have_mason then
-        all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-      end
 
-      local ensure_installed = {} ---@type string[]
-      for server, server_opts in pairs(servers) do
-        if server_opts then
-          server_opts = server_opts == true and {} or server_opts
-          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-          if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
-            setup(server)
-          else
-            ensure_installed[#ensure_installed + 1] = server
+      if have_mason then
+        -- Let mason-lspconfig handle setup automatically
+        local ensure_installed = {} ---@type string[]
+        for server, server_opts in pairs(servers) do
+          if server_opts then
+            server_opts = server_opts == true and {} or server_opts
+            -- Only add to mason if not explicitly disabled
+            if server_opts.mason ~= false then
+              ensure_installed[#ensure_installed + 1] = server
+            end
           end
         end
-      end
-
-      if have_mason then
         mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
+      else
+        -- Fallback: setup servers directly without mason
+        for server, server_opts in pairs(servers) do
+          if server_opts then
+            server_opts = server_opts == true and {} or server_opts
+            setup(server)
+          end
+        end
       end
     end,
   },
