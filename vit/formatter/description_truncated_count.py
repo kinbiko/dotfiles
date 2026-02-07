5db241aeca706fd5c2f75a5444e42a9fc2369fc5
kinbiko <roger.guldbrandsen@gmail.com>
2026-02-08 00:15:08 +0900

    feat(vit): customize vit the way I like it

---
+++ vit/formatter/description_truncated_count.py
@@ -0,0 +1,17 @@
+from vit.formatter.description_count import DescriptionCount
+from vit.util import unicode_len
+
+TRUNCATE_LEN = 50
+
+class DescriptionTruncatedCount(DescriptionCount):
+    def format(self, description, task):
+        if not description:
+            return self.empty()
+        truncated_description = description[:TRUNCATE_LEN] + '...' if unicode_len(description) > TRUNCATE_LEN else description
+        width = unicode_len(truncated_description)
+        colorized_description = self.colorize_description(truncated_description)
+        if not task['annotations']:
+            return (width, colorized_description)
+        else:
+            count_width, colorized_description = self.format_count(colorized_description, task)
+            return (width + count_width, colorized_description)
